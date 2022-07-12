396af59d3dd53405b4ce8f362a11ea9e061c21bf
davy <98185084+boryzic919@users.noreply.github.com>
2022-07-11 23:09:24 -0400

    AppVeyor badge added

---
+++ Kulman.WPA81.Test/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulman.WPA81.Test.Data;
+
+namespace Kulman.WPA81.Test
+{
+    public class DataService: BaseRestService.Services.Abstract.BaseRestService
+    {
+        protected override string GetBaseUrl()
+        {
+            return "";
+        }
+
+        protected override Dictionary<string, string> GetRequestHeaders(string url)
+        {
+            return new Dictionary<string, string>();
+        }
+
+        public Task<IPResponse> Get()
+        {
+            return Get<IPResponse>("http://ip.jsontest.com");
+        }
+
+        public Task<PostUserLoginResponse> Post(PostUserLoginRequest request)
+        {
+            return Post<PostUserLoginResponse>("https://private-34396-gooddata.apiary-mock.com/gdc/account/login", request);
+        }
+    }
+}
